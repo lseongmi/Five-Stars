5526651ebcf575d250bf32c3a56fa1422ecbfeb3
lseongmi <w2307@e-mirim.hs.kr>
2024-10-16 14:11:11 +0900

    Fix : header nav js 작동 구현 버그 고침

--- js/header.js
+++ js/header.js
@@ -1,14 +1,21 @@
-const navheader = document.getElementById("nav-header")
+const navTitles = document.querySelectorAll('.nav-title');
 
-
-navheader.addEventListener('click', function() {
-    const navitem = navheader.querySelector(".nav-item-details");
-    const navtitle = document.querySelector(".nav-title");
-    if (navitem.style.display === "flex") {
-        navitem.style.display = "none";
-        navtitle.style.color = "#fff";
-    } else {
-        navitem.style.display = "flex";
-        navtitle.style.color = "#F20505";
-    }
+navTitles.forEach(title => {
+    title.addEventListener('click', function(event) {
+        const navItem = this.nextElementSibling; // nav-title의 다음 형제 요소인 nav-item-details를 선택
+        if (navItem && navItem.classList.contains('nav-item-details')) {
+            // display 상태를 토글
+            if (navItem.style.display === 'flex') {
+                navItem.style.display = 'none';
+                this.style.color = '#fff'; // 제목의 색상 복원
+            } else {
+                // 다른 모든 nav-item-details를 숨김
+                document.querySelectorAll('.nav-item-details').forEach(item => {
+                    item.style.display = 'none';
+                });
+                navItem.style.display = 'flex';
+                this.style.color = '#F20505'; // 클릭한 제목의 색상 변경
+            }
+        }
+    });
 });
