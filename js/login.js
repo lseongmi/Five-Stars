626e9fc753b6f0b051e0f92f4daa7c4a782e9244
lseongmi <w2307@e-mirim.hs.kr>
2024-10-22 00:51:32 +0900

    Feat: musicplayer slide 구현

--- js/login.js
+++ js/login.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
 
+    // 로그인 폼 제출 이벤트
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
+
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
-        if (username === "t1fan" && password === "t1fan1234") {
-            localStorage.setItem("login", "true");
+        // 로그인 성공 처리 (무조건 성공)
+        localStorage.setItem("login", "true");
+
+        // 특정 값을 저장 (예: "특정 사용자 이름"만 저장)
+        if (username === "t1fan" && password === "t1fan1234") { // 특정값을 실제 조건으로 설정
             localStorage.setItem("username", username);
-            alert("로그인 성공");
+            localStorage.setItem("password", password);
             window.location.href = "../index.html";
-        } else {
-            alert("로그인 실패");
         }
+
+        alert("로그인 성공");
+        checkLoginStatus(); // 로그인 상태 업데이트
     });
 });
