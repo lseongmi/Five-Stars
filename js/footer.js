371b85b5ffbd2a876221a966f47e9a42b73d6b6e
lseongmi <w2307@e-mirim.hs.kr>
2024-11-25 09:19:55 +0900

    Fix: footer 반응형 버그 sns 이미지 버그 픽스

--- js/footer.js
+++ js/footer.js
@@ -1,18 +1,23 @@
 // 모든 footer-title과 footer-content 가져오기
 const footertitles = document.querySelectorAll(".footer-title");
+const snsimg = document.querySelector(".SNS-img");
 
 footertitles.forEach((title) => {
     title.addEventListener("click", function () {
         // 클릭된 footer-title의 부모에서 footer-content 찾기
         const footerContent = this.nextElementSibling;
 
-        // 현재 footer-content의 display 상태 확인
-        const currentDisplay = getComputedStyle(footerContent).display;
+        if (footerContent) {
+            // 현재 footer-content의 display 상태 확인
+            const currentDisplay = getComputedStyle(footerContent).display;
 
-        if (currentDisplay === "none") {
-            footerContent.style.display = "block"; // 열기
+            if (currentDisplay === "none") {
+                footerContent.style.display = "block"; // 열기
+            } else {
+                footerContent.style.display = "none"; // 닫기
+            }
         } else {
-            footerContent.style.display = "none"; // 닫기
+            console.error("footer-content가 발견되지 않았습니다.");
         }
     });
 });
